641399dcbb02c3efc14de8d404a57bb1dcf8cdc5
Cristhyan1 <cristhyan_1@hotmail.com>
2025-05-26 23:22:50 -0500

    nueva estructura

---
+++ db.py
@@ -0,0 +1,10 @@
+# db.py
+from flask_mysqldb import MySQL
+
+def init_mysql(app):
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = 'tupassword'
+    app.config['MYSQL_DB'] = 'aea_farmacia'
+    app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+    return MySQL(app)
